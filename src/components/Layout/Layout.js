14545fc3dd022ba5e956ce04acb86e89444daee3
Todor Vlasev <todor.vlasev@strypes.eu>
2020-04-05 19:14:29 +0300

    initial commit for burger builder app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import Wrapp from '../../hoc/Wrapp';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Wrapp>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Wrapp>
+        )
+    }
+}
+
+export default Layout;

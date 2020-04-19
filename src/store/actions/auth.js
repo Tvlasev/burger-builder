780aa0a58c2b2c4777f9db7d60e5ffd551108b97
Todor Vlasev <todor.vlasev@strypes.eu>
2020-04-19 23:53:24 +0300

    adding logout actions and reducers

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
 export const auth = (email, password, isSignup) => {
     const APIkey = "AIzaSyBrv03Encj73FhWDSH7bpkEimpaIn_4AJc";
     return dispatch => {
@@ -39,12 +53,11 @@ export const auth = (email, password, isSignup) => {
         }
         axios.post(url, authData)
             .then(response => {
-                console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
-                console.log(err);
-                dispatch(authFail(err));
+                dispatch(authFail(err.response.data.error));
             });
     };
 };

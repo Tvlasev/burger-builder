b36e52634e6a41e3fe1523968c7d3df11015d8d3
Todor Vlasev <todor.vlasev@strypes.eu>
2020-04-19 20:04:08 +0300

    adding authentication sign in and sign out and storing the token after login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -20,8 +23,28 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
+    const APIkey = "AIzaSyBrv03Encj73FhWDSH7bpkEimpaIn_4AJc";
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIkey}`;
+        if (!isSignup) {
+            console.log("a")
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${APIkey}`;
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };
